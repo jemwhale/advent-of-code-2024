842f5d4e5b052b9d0f84adb3a6ddd2fcfa53d457
Jack Whale <jemwhale@gmail.com>
2024-12-12 21:46:16 +0000

    2:1 solution

--- src/2/solution.ts
+++ src/2/solution.ts
@@ -7,36 +7,31 @@ import { readFileLinesSync } from "../lib/helpers.js";
 
 const lines = readFileLinesSync(import.meta.url, 'input-data.txt');
 
-// const isIncreasing = (level: number): boolean => {
+const isSafe = (report: number[]): boolean => {
+    const slicedReport = report.slice(0, -1);
 
-// }
-
-// const isDecreasing = (level: number): boolean => {
+    const isIncreasing = slicedReport.every((level, index) => {
+        return level < report[index + 1];
+    })
     
-// }
-
-// const getDiff = (currentLevel: number, nextLevel: number): number => {
+    const isDecreasing = slicedReport.every((level, index) => {
+        return level > report[index + 1];
+    })
     
-// }
+    const isGradual = slicedReport.every((level, index) => {
+        const diff = Math.abs(level - report[index + 1])
+        return diff >= 1 && diff <= 3;
+    })
 
-// const isSafe = (report: number[]): boolean => {
+    return (isIncreasing || isDecreasing) && isGradual;
+}
 
+const reports = lines.map((report) => {
+    return report.split(' ').map((level) => parseInt(level));
+});
 
-// }
+const solution1: number = reports.filter((report) => {
+  return isSafe(report)
+}).length;
 
-// const sumSafeReports = () => {
-//     return lines.filter((line) => {
-//         const report  = line.split(' ');
-//         let increase = true;
-//         let previousLevel = report[0];
-//         const safe = report.filter((level) => {
-            
-//             increase === true && (level )
-//         }).length;
-//         safe > 1;
-//     }).length
-// }
-
-console.log(lines[0].split(' '))
-
-// console.log(lines[0].split('   ')).slice(0, -1);
+console.log('Part 1 solution: ', solution1);
