0addc049a66d25a3206b4c7827e5488828b315af
Jack Whale <jemwhale@gmail.com>
2024-12-12 20:40:37 +0000

    more typescript

---
+++ src/2/solution.ts
@@ -0,0 +1,42 @@
+/**
+ * Problem
+ * - https://adventofcode.com/2024/day/2
+ */
+
+import { readFileLinesSync } from "../lib/helpers.js";
+
+const lines = readFileLinesSync(import.meta.url, 'input-data.txt');
+
+// const isIncreasing = (level: number): boolean => {
+
+// }
+
+// const isDecreasing = (level: number): boolean => {
+    
+// }
+
+// const getDiff = (currentLevel: number, nextLevel: number): number => {
+    
+// }
+
+// const isSafe = (report: number[]): boolean => {
+
+
+// }
+
+// const sumSafeReports = () => {
+//     return lines.filter((line) => {
+//         const report  = line.split(' ');
+//         let increase = true;
+//         let previousLevel = report[0];
+//         const safe = report.filter((level) => {
+            
+//             increase === true && (level )
+//         }).length;
+//         safe > 1;
+//     }).length
+// }
+
+console.log(lines[0].split(' '))
+
+// console.log(lines[0].split('   ')).slice(0, -1);
