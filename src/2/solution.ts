6d389b5255653381a2372c6058f9e7e5af32edfc
Jack Whale <jemwhale@gmail.com>
2024-12-14 13:18:55 +0000

    2:2 solution

--- src/2/solution.ts
+++ src/2/solution.ts
@@ -35,3 +35,28 @@ const solution1: number = reports.filter((report) => {
 }).length;
 
 console.log('Part 1 solution: ', solution1);
+
+const isSafeTolerance = (report: number[]): boolean => {
+
+    if (isSafe(report)) return true
+
+    let inTolerance = false
+
+    for (let i = 0; i < report.length; i++) {
+        let splicedReport = [...report];
+        splicedReport.splice(i, 1);
+        let safeReport = isSafe(splicedReport);
+        if (safeReport) {
+            inTolerance = true;
+            break;
+        }
+    }
+
+    return inTolerance
+}
+
+const solution2: number = reports.filter((report) => {
+    return isSafeTolerance(report)
+  }).length;
+  
+  console.log('Part 2 solution: ', solution2);
