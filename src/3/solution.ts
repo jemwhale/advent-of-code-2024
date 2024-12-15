96ad82b515bcae9319173c13ff56d57fe1bc7618
Jack Whale <jemwhale@gmail.com>
2024-12-15 21:49:18 +0000

    3:2 solution

--- src/3/solution.ts
+++ src/3/solution.ts
@@ -10,10 +10,26 @@ const fileString = readFileStringSync(import.meta.url, 'input-data.txt');
 const targetRegex = /mul\(\d{1,3}\,\d{1,3}\)/g;
 const targetMatches = fileString.matchAll(targetRegex);
 let total = 0;
+const multiplyOperation= (mulStr: string): number => {
+    const num1 = parseInt(mulStr.slice(mulStr.indexOf('(') + 1, mulStr.indexOf(',')));
+    const num2 = parseInt(mulStr.slice(mulStr.indexOf(',') + 1, mulStr.indexOf(')')));
+    return num1 * num2
+}
 if (targetMatches) for (const element of targetMatches) {
-    const num1 = parseInt(element[0].slice(element[0].indexOf('(') + 1, element[0].indexOf(',')));
-    const num2 = parseInt(element[0].slice(element[0].indexOf(',') + 1, element[0].indexOf(')')));
-    total += num1 * num2
+    total += multiplyOperation(element[0])
 }
 const solution1 = total;
 console.log(solution1);
+total = 0
+
+const operatorRegex = /mul\(\d{1,3}\,\d{1,3}\)|do\(\)|don't\(\)/g;
+const targetMatches2 = fileString.matchAll(operatorRegex);
+let enableOperations = true;
+if (targetMatches2) for (const element of targetMatches2) {
+    let operation = element[0] === 'do()' || element[0] === "don't()" ? true : false;
+    if (element[0] === 'do()') enableOperations = true;
+    if (element[0] === "don't()") enableOperations = false;
+    if (!operation && enableOperations) total += multiplyOperation(element[0]);
+}
+const solution2 = total;
+console.log(solution2);
