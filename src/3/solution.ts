8e42a3b0b32be203d940ce82041e94e45b01374b
Jack Whale <jemwhale@gmail.com>
2024-12-15 21:25:06 +0000

    3:1 solution

--- src/3/solution.ts
+++ src/3/solution.ts
@@ -7,4 +7,13 @@ import { readFileStringSync } from "../lib/helpers.js";
 
 const fileString = readFileStringSync(import.meta.url, 'input-data.txt');
 
-console.log(fileString);
+const targetRegex = /mul\(\d{1,3}\,\d{1,3}\)/g;
+const targetMatches = fileString.matchAll(targetRegex);
+let total = 0;
+if (targetMatches) for (const element of targetMatches) {
+    const num1 = parseInt(element[0].slice(element[0].indexOf('(') + 1, element[0].indexOf(',')));
+    const num2 = parseInt(element[0].slice(element[0].indexOf(',') + 1, element[0].indexOf(')')));
+    total += num1 * num2
+}
+const solution1 = total;
+console.log(solution1);
